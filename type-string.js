2c2243bd37adf78d69ef464abbf2c35a22fed576
Esther de Moura <106841785+esthertamires@users.noreply.github.com>
2023-03-08 16:55:43 -0300

    primeira aula de javascript

---
+++ type-string.js
@@ -0,0 +1,14 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!"
+const SetringDeNumeros = "34567";
+
+const citacao = 'Meu nome é ';
+const meuNome = "Esther"
+
+
+// concatenação (+)
+
+console.log(citacao + meuNome)
+
+//tamplete string ou templete literal
